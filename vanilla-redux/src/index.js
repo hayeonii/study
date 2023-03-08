b99c143f792a8f3edfaab9ae43117b9e80f02aca
hayeonii <hayexn51@gmail.com>
2023-03-08 19:33:35 +0900

    store와 reducer의 이해

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,22 +1,17 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
+// const reducer = () => {};
 
-const updateText = () => {
-  number.innerHTML = count;
+// const store = createStore(reducer);
+
+const countModifier = () => {
+  return "hello";
 };
 
-const handleAdd = () => {
-  count++;
-  updateText();
-};
+const countStore = createStore(countModifier);
 
-const handleMinus = () => {
-  count--;
-  updateText();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countStore.getState());
