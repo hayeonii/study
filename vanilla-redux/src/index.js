f34b42094e03aec8fa5287f1bb1986f94715c2e5
hayeonii <hayexn51@gmail.com>
2023-03-08 20:01:48 +0900

    actions

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -9,10 +9,26 @@ const number = document.querySelector("span");
 // const store = createStore(reducer);
 
 // state = 0 : 초기값 설정
-const countModifier = (state = 0) => {
-  return state;
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 const countStore = createStore(countModifier);
+
+// action 보내기, action은 object여야함
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 
 console.log(countStore.getState());
