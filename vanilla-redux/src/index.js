1087c2a8a09a6dd4d584182e38e3cbef9b7de2ec
hayeonii <hayexn51@gmail.com>
2023-03-08 20:13:41 +0900

    subscribe

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -3,11 +3,13 @@ import { createStore } from "redux";
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
+number.innerText = 0;
 
 // const reducer = () => {};
 
 // const store = createStore(reducer);
 
+// data를 수정하는 유일한 곳
 // state = 0 : 초기값 설정
 const countModifier = (count = 0, action) => {
   console.log(count, action);
@@ -22,13 +24,17 @@ const countModifier = (count = 0, action) => {
 
 const countStore = createStore(countModifier);
 
-// action 보내기, action은 object여야함
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
 
-console.log(countStore.getState());
+// store에 변화가 있을 때마다 감지해서 불려짐
+countStore.subscribe(onChange);
+
+// eventListener의 두번째 인자는 익명함수 or 함수
+add.addEventListener("click", () => {
+  countStore.dispatch({ type: "ADD" });
+});
+minus.addEventListener("click", () => {
+  countStore.dispatch({ type: "MINUS" });
+});
