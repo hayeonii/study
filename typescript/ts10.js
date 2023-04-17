ebb4df29014ff780b2984ddead4dd2e987b5f20c
hayeonii <hayexn51@gmail.com>
2023-04-18 01:46:31 +0900

    interface

---
+++ typescript/ts10.js
@@ -0,0 +1,24 @@
+// interface로 타입 지정
+var 네모 = {
+    color: "red",
+    width: 100,
+};
+var 학생 = { name: "kim" };
+var 선생 = { name: "kim", age: 20 };
+var 상품 = {
+    brand: "Samsung",
+    serialNumber: 1360,
+    model: ["TV", "phone"],
+};
+var 장바구니 = [
+    { product: "청소기", price: 7000 },
+    { product: "삼다수", price: 800 },
+];
+var question101 = {
+    plus: function (x, y) {
+        return x + y;
+    },
+    minus: function (x, y) {
+        return x - y;
+    },
+};
